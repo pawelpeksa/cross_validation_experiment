b5934d3d18faa52e8ae2c3169805c42ce44d083f
pawelpeksa <pmpeksa@gmail.com>
2017-07-20 20:11:10 +0200

    experim1

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.datasets import make_classification
 import time
 
+import matplotlib.pyplot as plt
+
 N_SAMPLES = 100000
 N_HOLDOUT = 5000
 
@@ -21,10 +23,13 @@ def main():
     scores4 = []
     scores5 = []
 
-    n_runs = 100
+    n_runs = 5
 
-    x_all, y_all = make_classification(n_samples=N_SAMPLES, n_features=10, n_redundant=0)
-
+    x_all, y_all = make_classification(n_samples=N_SAMPLES, n_features=30, n_redundant=0)
+    
+    # plt.scatter(x_all[:, 0], x_all[:, 1], marker='o', c=y_all)
+    # plt.show()
+    
     x_holdout, x_without_holdout, y_holdout, y_without_holdout = train_test_split(x_all, y_all, train_size=N_HOLDOUT, random_state=int(int(time.time())))
 
     svm_c = calc_svm_parameter(x_holdout, y_holdout)
@@ -69,14 +74,25 @@ def run_example(x_all, y_all, x_holdout, y_holdout, x_without_holdout, y_without
 
     x_train, x_test, y_train, y_test = train_test_split(x_holdout, y_holdout, test_size=0.3, random_state=int(int(time.time())))
 
-    classifier = svm.SVC(kernel='linear', C=svm_c).fit(x_train, y_train)
-    classifierCV = svm.SVC(kernel='linear', C=svm_c)
+    classifier = svm.SVC(gamma=1, C=10).fit(x_train, y_train)
+    classifierCV = svm.SVC(gamma=1, C=10)
+#    classifier = svm.SVC(kernel='linear', C=svm_c).fit(x_train, y_train)
+#    classifierCV = svm.SVC(kernel='linear', C=svm_c)
 
     score1 = classifier.score(x_test, y_test) # score on test data set
-    score2 = np.mean(cross_val_score(classifierCV, x_holdout, y_holdout, cv=10)) # estimate accuracy using cv with 5000 samples
+
+    tmp, x_holdout, tmp, y_holdout = train_test_split(x_holdout, y_holdout, test_size=0.9999999, random_state=int(int(time.time())))
+
+    cvscore = cross_val_score(classifierCV, x_holdout, y_holdout, cv=10)
+    score2 = np.mean(cvscore)
+#    score2 = np.mean(cross_val_score(classifierCV, x_holdout, y_holdout, cv=10)) # estimate accuracy using cv with 5000 samples
     score3 = classifier.score(x_without_holdout, y_without_holdout) # accuracy over entire dataset without holdout samples (5000)
     score4 = classifier.score(x_train, y_train) # accuracy over train samples (should be the highest)
     score5 = classifier.score(x_all, y_all) # accuract over entire dataset
+
+    print "SVM: ",score1
+    print "SVM CV: ",score2
+    print "SVM CV: ", cvscore
 
     return score1, score2, score3, score4, score5
 
