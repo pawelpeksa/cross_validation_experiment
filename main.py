0021cadb2d33d248357e58b2b4c54d04644e746a
pawelpeksa <pmpeksa@gmail.com>
2017-07-23 12:42:21 +0200

    refactor + expand to more ml methods

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import numpy as np
-import sys
 from SVM_Optimizer import SVM_Optimizer
+from ANN_Optimizer import ANN_Optimizer
+from DecisionTree_Optimizer import DecisionTree_Optimizer
+from RandomForest_Optimizer import RandomForest_Optimizer
+from MethodsConfiguration import MethodsConfiguration
 
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.datasets import make_classification
 from sklearn.utils import shuffle
+
+from copy import deepcopy
 import time
+import numpy as np
 
 N_SAMPLES = 100000
 
@@ -32,7 +37,7 @@ def main():
         diff_holdout_arr = list()
 
         for i in range(11):
-            diff_cv, diff_holdout = run_with_nsample(x_all, y_all, n_samples)
+            diff_cv, diff_holdout = optimize_and_score(x_all, y_all, n_samples)
             diff_cv_arr.append(diff_cv)
             diff_holdout_arr.append(diff_holdout)
 
@@ -42,35 +47,17 @@ def main():
                             "\t" + str(np.mean(diff_holdout_arr)) + "\t" + str(np.std(diff_holdout_arr)) +\
                             "\n")
 
-def run_with_nsample(x_all, y_all, holdout_n):
-    scores1 = []
-    scores2 = []
-    scores3 = []
-    scores4 = []
-    scores5 = []
 
-    # n_runs = 2
-    #
-    # for i in range(n_runs):
-
+def optimize_and_score(x_all, y_all, holdout_n):
     x_holdout, x_without_holdout, y_holdout, y_without_holdout = train_test_split(x_all, y_all, train_size=holdout_n, random_state=get_seed())
 
     shuffle(x_holdout, y_holdout, random_state=get_seed())
     x_train, x_test, y_train, y_test = train_test_split(x_holdout, y_holdout, test_size=0.3, random_state=get_seed())
 
-    svm_c = calc_svm_parameter(x_train, y_train, x_test, y_test)
+    config = determine_parameters_all(x_train, y_train, x_test, y_test)
 
-    acc1, acc2, acc3, acc4, acc5 = run_example(x_all, y_all, x_holdout, y_holdout, x_without_holdout,y_without_holdout, x_train, y_train, x_test, y_test, svm_c)
-
-    scores1 = np.append(scores1, acc1)
-    scores2 = np.append(scores2, acc2)
-    scores3 = np.append(scores3, acc3)
-    scores4 = np.append(scores4, acc4)
-    scores5 = np.append(scores5, acc5)
-
-    # print 'progress:', ((i + 1) / float(n_runs)) * 100, '%'
-    #
-    # print_result(scores1, scores2, scores3, scores4, scores5, holdout_n)
+    clf = svm.SVC(kernel='linear', C=config.svm.C)
+    acc1, acc2, acc3, acc4, acc5 = score_model(x_all, y_all, x_holdout, y_holdout, x_without_holdout, y_without_holdout, x_train, y_train, x_test, y_test, clf)
 
     print holdout_n, " holdout test sample:", acc1
     print "cv10 on holdout set:", acc2
@@ -81,62 +68,27 @@ def run_with_nsample(x_all, y_all, holdout_n):
     return abs(acc3 - acc2), abs(acc3 - acc1)
 
 
-def print_result(scores1, scores2, scores3, scores4, scores5, holdout_n):
-    print "\n\nscores:\n"
+def determine_parameters_all(x_train, y_train, x_test, y_test):
+    print "determine parameters"
+    config = MethodsConfiguration()
 
-    print holdout_n, " holdout test sample:", np.mean(scores1), " +- ", np.std(scores1)
-    print "cv10 on holdout set:", np.mean(scores2), " +- ", np.std(scores2)
-    print "entire dataset without holdout:", np.mean(scores3), " +- ", np.std(scores3)
-    print "entire dataset", np.mean(scores5), " +- ", np.std(scores5)
-    print "train dataset:", np.mean(scores4), " +- ", np.std(scores4)
+    config.svm.C = determine_parameters(SVM_Optimizer(x_train, y_train, x_test, y_test))
+    # config.ann.hidden_neurons, config.ann.solver, config.ann.alpha = determine_parameters(ANN_Optimizer(x_train, y_train, x_test, y_test))
+    # config.decision_tree.max_depth = determine_parameters(DecisionTree_Optimizer(x_train, y_train, x_test, y_test))
+    # config.random_forest.max_depth, config.random_forest.n_estimators = determine_parameters(RandomForest_Optimizer(x_train, y_train, x_test, y_test))
 
-    cvErrList.append(np.mean(scores2))
-    holdoutErrList.append(np.mean(scores1))
-    realErrList.append(np.mean(scores3))
-
-    cvErrListStd.append(np.std(scores2))
-    holdoutErrListStd.append(np.std(scores1))
-    realErrListStd.append(np.std(scores3))
+    return config
 
 
-def calc_svm_parameter(x_train, y_train, x_test, y_test):
-    optimizer = SVM_Optimizer(x_train, y_train, x_test, y_test)
-
-    svm_c = optimizer.optimize()
-    # svm_c = 0.373874872174 # N_HOLDOUT = 2000
-    # svm_c = 0.0761514827158  # N_HOLDOUT = 5000
-
-    print svm_c
-
-    return svm_c
+def determine_parameters(optimizer):
+    print 'determine parameters', optimizer.__class__.__name__
+    return optimizer.optimize()
 
 
+def score_model(x_all, y_all, x_holdout, y_holdout, x_without_holdout, y_without_holdout, x_train, y_train, x_test, y_test, classifier):
+    classifierCV = deepcopy(classifier)
 
-from sklearn.neural_network import MLPClassifier
-
-def run_example(x_all, y_all, x_holdout, y_holdout, x_without_holdout, y_without_holdout, x_train, y_train, x_test, y_test, svm_c):
-
-    classifier = svm.SVC(kernel='linear', C=svm_c).fit(x_train, y_train)
-    classifierCV = svm.SVC(kernel='linear', C=svm_c)
-
-    # classifier = MLPClassifier(solver='adam',
-    #               max_iter=1000,
-    #               alpha=0.01,
-    #               hidden_layer_sizes=(5,),
-    #               random_state=1,
-    #               learning_rate='adaptive').fit(x_train, y_train)
-    #
-    # classifierCV = MLPClassifier(solver='adam',
-    #                            max_iter=1000,
-    #                            alpha=0.01,
-    #                            hidden_layer_sizes=(5,),
-    #                            random_state=1,
-    #                            learning_rate='adaptive')
-
-    # classifier = DecisionTreeClassifier(max_depth=7).fit(x_train, y_train)
-    #
-    # classifierCV  = DecisionTreeClassifier(max_depth=7)
-
+    classifier.fit(x_train, y_train)
 
     score1 = classifier.score(x_test, y_test) # score on test data set
     scoresCV = cross_val_score(classifierCV, x_holdout, y_holdout, cv=10)
@@ -145,8 +97,6 @@ def run_example(x_all, y_all, x_holdout, y_holdout, x_without_holdout, y_without
     score4 = classifier.score(x_train, y_train) # accuracy over train samples (should be the highest)
     score5 = classifier.score(x_all, y_all) # accuract over entire dataset
 
-    # print scoresCV
-    
     return score1, score2, score3, score4, score5
 
 
