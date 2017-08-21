a4c673e52d1e72fe7bbbffd5abd52483b8dedb20
pawellll <pmpeksa@gmail.com>
2017-08-21 15:48:01 +0200

    cosmetic updates

--- Configuration.py
+++ Configuration.py
@@ -2,11 +2,10 @@ class Configuration:
     def __init__(self):
         pass
 
-    # HYPEROPT_EVALS_PER_SEARCH = 2000
-    HYPEROPT_EVALS_PER_SEARCH = 100
-    ANN_MAX_ITERATIONS = 100
+    HYPEROPT_EVALS_PER_SEARCH = 2000
+    ANN_MAX_ITERATIONS = 1000
     ANN_OPIMIZER_MAX_ITERATIONS = 500
 
     N_SAMPLES = 100000
-    RUNS_FOR_SAMPLE = 5
-    n_samples_arr = [50, 100, 150, 200, 250, 300]
+    RUNS_FOR_SAMPLE = 10
+    n_samples_arr = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
